5d19479821e05dfcb6d8ecd794ef6b292fcb270f
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-01 12:47:52 +0200

    created one more component and import Autocomplete and TextField—from the Material UI (MUI) library. These are used to build modern, responsive form inputs in a React application.

---
+++ src/components/ExperimentSelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Autocomplete,TextField } from '@mui/material'
+const ExperimentSelector = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExperimentSelector
