8296fde85f3a888eefd45e0bdee6cf4b1cf46f81
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-01 12:12:25 +0200

    used Papa object for parsing data.

--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -3,6 +3,17 @@ import Papa from 'papaparse';
 const FileUploader = ({onDataParsed}) => {
     const handleFileChange = (event) => {
           const file = event.target.files[0];
+      Papa.parse(file,{
+        header:true,
+        dynamicTyping: true,
+        complete:(results) => {
+            const cleaned = results.data.filter(row =>
+                   row.experiment_id && row.metric_name && row.step != null && row.value != null
+
+             );
+             onDataParsed(cleaned);
+        }
+      })    
     }
   return (
     <div>
