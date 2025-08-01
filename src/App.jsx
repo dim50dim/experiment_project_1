1fc42b1d57a3ac1fef923ce0e5ef7d51b785ab70
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-01 12:40:17 +0200

    implemented FileUploader to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import {Container, Typography} from '@mui/material';
 import './App.css'
+import FileUploader from './components/FileUploader';
 
 function App() {
 const [parsedDate, setParsedData] = useState([]);
 
   return (
     <>
-  
+         <FileUploader/>
     </>
   )
 }
