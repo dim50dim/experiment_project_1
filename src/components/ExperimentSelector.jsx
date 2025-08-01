954249134282b9f9aa2de81870b042a6e9b872ea
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-01 12:48:48 +0200

    used props - experiments, selected ,onChange

--- src/components/ExperimentSelector.jsx
+++ src/components/ExperimentSelector.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Autocomplete,TextField } from '@mui/material'
-const ExperimentSelector = () => {
+const ExperimentSelector = ({experiments, selected,onChange}) => {
   return (
     <div>
       
